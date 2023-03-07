64646f3c6876f57d0c90cff9c7bccdaca22dc5eb
heony <oueya1479@naver.com>
2023-03-07 20:00:55 +0900

    user commit

--- src/main/java/com/project/theglory/domain/entity/User.java
+++ src/main/java/com/project/theglory/domain/entity/User.java
@@ -1,12 +1,15 @@
 package com.project.theglory.domain.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,4 +24,16 @@ public class User {
 	private Long userId;
 	private String userNm;
 	private String profile;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Favorite> favorities;
+
+	@Builder
+	public User(Long userId, String userNm, String profile, List<Favorite> favorities) {
+		this.userId = userId;
+		this.userNm = userNm;
+		this.profile = profile;
+		this.favorities = favorities;
+	}
+	
 }
