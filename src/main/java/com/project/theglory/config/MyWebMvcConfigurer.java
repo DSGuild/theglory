3b8c9cd8ab163da38c35e8f8b623f3201fe2a312
heony <oueya1479@naver.com>
2023-03-10 19:22:36 +0900

    cors 무시

---
+++ src/main/java/com/project/theglory/config/MyWebMvcConfigurer.java
@@ -0,0 +1,19 @@
+package com.project.theglory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .exposedHeaders("Authorization")
+            .allowCredentials(false)
+            .maxAge(3600);
+    }
+}
